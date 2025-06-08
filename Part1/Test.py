845b6e696f8a2c259555a409a9dc6cf384c70397
Martian <1324675920@qq.com>
2025-06-08 14:52:48 +0800

    part2 built

--- Part1/Test.py
+++ Part1/Test.py
@@ -1,4 +1,6 @@
 import cv2
+from cv2.typing import Scalar
+
 # opencv 图像处理基础知识
 
 # 读取图片
@@ -9,3 +11,5 @@ cv2.imshow('Demo',img)
 # 等待显示
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+s = Scalar(0,0,255)

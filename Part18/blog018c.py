e0778032c4aa959339c4f29da6d65052e8ea66b0
Martian <1324675920@qq.com>
2025-06-13 14:42:23 +0800

    Part18 was completed

--- Part18/blog018c.py
+++ Part18/blog018c.py
@@ -3,14 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 读取图像
-src = cv2.imread('tanjilo.jpg')
-src = cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
+src = cv2.imread('TomJerry.jpg')
+
+# 获取BGR三个通道
+b,g,r = cv2.split(src)
 
 # 绘制直方图
-plt.hist(src.ravel(),256)
+plt.figure('炭治郎')
+# 蓝色分量
+plt.hist(b.ravel(),bins = 256,density = 1,facecolor = 'b',edgecolor = 'b',alpha = 0.75)
+# 绿色分量
+plt.hist(g.ravel(),bins = 256,density = 1,facecolor = 'g',edgecolor = 'g',alpha = 0.75)
+# 红色分量
+plt.hist(r.ravel(),bins = 256,density = 1,facecolor = 'r',edgecolor = 'r',alpha = 0.75)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
+
 
 # 显示原始图像
 cv2.imshow('src',src)
